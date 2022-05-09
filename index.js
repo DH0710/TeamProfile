bdfb76ec1a20c3a2f95a76f0c762ac64160c75be
blur <damienhebert710@gmail.com>
2022-05-08 23:20:25 -0500

    adding manager questions and engineer questions as well' clear quit quit clear '

--- index.js
+++ index.js
@@ -1,22 +1,92 @@
 const inquirer = require('inquirer');
 const jest = require("jest")
 const fs = require('fs');
+const generateHTML = require('./utils/generateHTML.js')
 
-const questions = () => {
+const Manager = require('./lib/manager.js')
+const Engineer = require('./lib/engineer.js')
+const Intern = require('./lib/intern.js')
+
+
+let isTeamComplete = false
+
+const validateInput = (userInput) => {
+    if (userInput === '') {
+        return ('Please type in your answer before continuing')
+    } else {
+        return true;
+    }
+
+};
+
+const init = async () => {
+    await createManager();
+    while (isTeamComplete) {
+
+        const employeeTypeQuestion = [
+            {
+                type: 'list',
+                message: 'Please select the employee type you wish to add',
+                name: 'employeeType',
+                choices: [
+                    {
+                        name: 'Engineer',
+                        value: 'engineer',
+                        short: 'Engineer'
+                    },
+                    {
+                        name: 'Intern',
+                        value: 'intern',
+                        short: 'Intern',
+                    },
+                    {
+                        name: 'None',
+                        value: 'none',
+                        short: 'None',
+                    },
+                ]
+            }
+        ];
+
+
+
+    }
+}
+
+const { employeeType } = await inquirer.prompt(employeeTypeQuestion);
+
+if (employeeType === 'none') {
+    isTeamComplete = true;
+
+} else {
+    if (employeeType === 'engineer') {
+        await createEngineer();
+    }
+    if (employeeType === 'intern') {
+        await createIntern();
+    }
+}
+
+const HTML = generateHTML(employees);
+fs.writeFileSync('team-profile.html', HTML, (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('HTML file created');
+    }
+});
+
+
+
+
+const managerQuestions = () => {
 
     return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('');
-                    return false;
-                }
-            }
+            message: 'What is your Manager name?',
+            validate: validateInput,
         }
     ]),
 
@@ -42,7 +112,43 @@ const questions = () => {
     },
     {
         type: 'input',
-        name: 'Manager',
+        name: 'officeNumber',
         message: 'What is your office Number?',
     }
+}
+
+const managerAnswers = await inquirer.prompt(managerQuestions);
+
+const manager = new Manager(managerAnswers);
+
+employees.push(manager)
+
+
+const createEngineer = async () => {
+
+    const engineerQuestions = [
+        {
+            type: 'input', 
+            message: 'PLease enter Engineer name', 
+            name: 'name',
+            validate: validateInput
+        }, 
+        {
+            type:'input', 
+            message:'Please enter engineer email',
+            name: 'email', 
+            validate: validateInput,
+        }, 
+        {
+            type: 'input',
+            message: 'Enter engineer Github profile', 
+            name: 'email', 
+            validate: validateInput, 
+        }, 
+        {
+            type:'input',
+            message:'Enter engineer ID', 
+            name: 'ID'
+        }, 
+    ]
 }
